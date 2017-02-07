4900098a23716f4e66cce81e4d738ff79fabbfc9
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-02-06 22:10:29 -0500

    Refactor

--- Encryption.java
+++ Encryption.java
@@ -1,25 +1,23 @@
-/**
+/*
  * Bailey Thompson
- * Encryption (1.1.5)
- * 8 January 2017
- * Info: This  encryption  program  takes  a user's text, tells the user the text when it is encrypted, then returns the
- * Info: encrypted  text to plain text. The plain text obviously has one part: the text. However, the encrypted text has
- * Info: four  parts.  The  first  part  is  the main encryption characters. The second part is the encryption code. The
- * Info: encryption  code  is  randomly  generated,  but  it is the same amount of characters as the plain text that the
- * Info: user  inputs.  When  these  numbers  are  generated,  they  are  then  added to the plain text to form the main
- * Info: encryption  characters.  The third part is the buffer, it is a set of random characters, and the buffer is of a
- * Info: random  size. However, the buffer is determined by the fourth part: the buffer number. The buffer number is one
- * Info: character  long,  but  it  determines the amount of buffer characters. The whole point of the buffer is to slow
- * Info: down  the  process  of  determining  the  code, the buffer number is needed in order to determine the amount of
- * Info: buffer  characters  there  are.  The  main  encryption characters and encryption code work together just as the
- * Info: buffer  and  the  buffer number work together. To keep adding security to the encryption code, more buffers and
- * Info: encryption  codes  can  be  added;  however, it would add too much to the length of the encrypted text, and not
- * Info: offer as much protection as to justify the extra length.
- * Extra: The  reason  why the encryption and decryption are separate private voids, and the only global variable is the
- * Extra: String  word is to make it more realistic, as if the encryption and decryption were separate programs operated
- * Extra: by different people sending information back and forth.
+ * Encryption (1.2.0)
+ * 6 February 2017
+ * This encryption program takes a user's text, tells the user the text when it is encrypted, then returns the encrypted
+ * text to plain text. The plain text obviously has one part: the text. However, the encrypted text has four parts. The
+ * first part is the main encryption characters. The second part is the encryption code. The encryption code is randomly
+ * generated, but it is the same amount of characters as the plain text that the user inputs. When these numbers are
+ * generated, they are then added to the plain text to form the main encryption characters. The third part is the
+ * buffer, it is a set of random characters, and the buffer is of a random size. However, the buffer is determined by
+ * the fourth part: the buffer number. The buffer number is one character long, but it determines the amount of buffer
+ * characters. The whole point of the buffer is to slow down the process of determining the code, the buffer number is
+ * needed in order to determine the amount of buffer characters there are. The main encryption characters and encryption
+ * code work together just as the buffer and the buffer number work together. To keep adding security to the encryption
+ * code, more buffers and encryption codes can be added; however, it would add too much to the length of the encrypted
+ * text, and not offer as much protection as to justify the extra length.
+ * The reason why the encryption and decryption are separate private voids, and the only global variable is the String
+ * word is to make it more realistic, as if the encryption and decryption were separate programs operated by different
+ * people sending information back and forth.
  */
-package encryption;
 
 import java.util.Arrays;
 import javax.swing.JOptionPane;
@@ -50,7 +48,7 @@ public class Encryption {
             System.exit(0);
         }
         int length = word.length(), random;
-        int buffer = ((int) (Math.random() * ((length - 0) + 0))) + 0;
+        int buffer = (int) (Math.random() * length);
         char letter[] = new char[((length * 2) + buffer + 1)];
         letter[((length * 2) + buffer)] = (char) buffer;
         for (int i = 0; i < length; i++) {
@@ -65,11 +63,11 @@ public class Encryption {
         word = String.valueOf(letter);
         if (length > 40) {
             check = JOptionPane.showConfirmDialog(null, "This is the encrypted message:\n" + "<html><body width='1000'>"
-                    + word + "\nTo decrypt the message, press ok.", "Encryption Program", JOptionPane.OK_CANCEL_OPTION,
+                            + word + "\nTo decrypt the message, press ok.", "Encryption Program", JOptionPane.OK_CANCEL_OPTION,
                     JOptionPane.PLAIN_MESSAGE);
         } else {
             check = JOptionPane.showConfirmDialog(null, "This is the encrypted message:\n" + word
-                    + "\nTo decrypt the message, press ok.", "Encryption Program", JOptionPane.OK_CANCEL_OPTION,
+                            + "\nTo decrypt the message, press ok.", "Encryption Program", JOptionPane.OK_CANCEL_OPTION,
                     JOptionPane.PLAIN_MESSAGE);
         }
         if (check != 0) {
